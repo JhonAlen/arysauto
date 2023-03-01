a3754a9d6b3d5230b87ef1ace0dcfdc7e8f117df
Franjhe <franjhely.andre13@gmail.com>
2023-03-01 17:22:58 -0400

    actualizacion de club

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -42,10 +42,10 @@ export class SignInComponent implements OnInit {
     }
     this.authenticationService.login(form.xemail, form.xcontrasena).pipe(first()).subscribe((data : any) => {
       this.loading = false;
-      if(data.data.ctipo_sistema){
+   
         if(data.data.ctipo_sistema == 1 ){ this.router.navigate(['/dahsboard']).then(() =>{ window.location.reload(); });}
-        else if(data.data.ctipo_sistema == 2){ this.router.navigate(['/home']).then(() =>{ window.location.reload(); }); }
-      }
+        else{ this.router.navigate(['/home']).then(() =>{ window.location.reload(); }); }
+     
 
     },
     
