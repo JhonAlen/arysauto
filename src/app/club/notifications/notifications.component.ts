52c5a211a6ec8aae4996b9349bf84f2076c6c738
Franjhe <franjhely.andre13@gmail.com>
2023-03-08 09:28:33 -0400

    actualizacion de club

--- src/app/club/notifications/notifications.component.ts
+++ src/app/club/notifications/notifications.component.ts
@@ -12,11 +12,12 @@ import { AuthenticationService } from '@services/authentication.service';
 })
 export class NotificationsComponent implements OnInit {
 
-  DataUser : FormGroup
+  servicePlanContract : FormGroup
   submitted = false;
   message : any;
   currentUser;
-  DataClient : any = [];
+  ListTypeService : any = [];
+  ListService : any = []
 
 
   constructor(
@@ -28,6 +29,15 @@ export class NotificationsComponent implements OnInit {
 
   ngOnInit() {
 
+    this.servicePlanContract = this.formBuilder.group({
+      ctiposervcicio:  [''],
+      cservicio:  [''],
+      cpais:  [''],
+      cestado:  [''],
+      ccuidad :  [''],
+      cproveedor :  [''],
+    });
+
   this.currentUser = this.authenticationService.currentUserValue;
   let plandata = {
     cpais: this.currentUser.data.cpais,
@@ -36,41 +46,24 @@ export class NotificationsComponent implements OnInit {
   this.http.post(environment.apiUrl + '/api/club/Data/Client/Plan', plandata).subscribe((response : any) => {
       
       let DataTypeServiceI = response.data.listTypeService
-      let DataServiceI = response.data.listService
-
-
       const DataTypeServiceP = DataTypeServiceI.filter
       ((data, index, j) => index === j.findIndex((t) => (t.ctiposervicio === data.ctiposervicio && t.xtiposervicio === data.xtiposervicio)))
 
-      const container = document.getElementById("accordionFlushExample");
-
-      const html = DataTypeServiceP.map(item => `
-      <div class="accordion-item">
-        <h2 class="accordion accordion-flush" id="${item.ctiposervicio}">
-          <button class="accordion-button collapsed" 
-          type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" 
-          aria-expanded="false" aria-controls="flush-collapseOne"> ${item.xtiposervicio}
-          </button>
-        </h2>
-
-      `).join('');
-    container.innerHTML = html;
-      // const containerF = document.querySelector(".accordion-flush");
-
-      // const htmlF = DataServiceI.map(service => `
-      //   <div id="${service.ctiposervicio}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-      //     <div class="accordion-body">${service.xservicio}</div>
-      //   </div>
-      // </div>
-      // `).join('');
-      // containerF.innerHTML = htmlF;
-
-  },
+      this.ListTypeService = DataTypeServiceP
+      this.ListService = response.data.listService
+  }
 
   );
   }
 
-  getdataservice(DataTypeServiceP){}
+  getdataservice(ctiposervicio:any){
+    
+    console.log(ctiposervicio)
+  }
+
+  Solicitud(cservicio:any){
+    console.log(cservicio)
+  }
 }
 
 
